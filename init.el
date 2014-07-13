cc7f6ce01e9fa6e495016491df71ef7e076befcc
winxton <winxton@gmail.com>
2014-07-12 20:19:51 -0400

    add auto complete

--- init.el
+++ init.el
@@ -42,3 +42,10 @@
 
 ;; Load stuff on demand
 (autoload 'auto-complete-mode "auto-complete" nil t)
+
+;; autocomplete
+
+(add-to-list 'load-path "~/.emacs.d/")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d//ac-dict")
+(ac-config-default)
